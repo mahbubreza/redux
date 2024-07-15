540f190e1204e9e4bd0d5ed8cda20423195afc47
mahbubreza <mahbubrezachoudhury@gmail.com>
2024-07-15 17:23:57 +0600

    redux first commit

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import "./App.css";
+import Counter from "./Components/Counter";
+import Stats from "./Components/Stats";
+
+const initialCounters = [
+  {
+    id: 1,
+    value: 0,
+  },
+  {
+    id: 2,
+    value: 0,
+  },
+];
+
+function App() {
+  const [counters, setCounters] = useState(initialCounters);
+
+  const totalCount = counters.reduce((sum, current) => sum + current.value, 0);
+
+  const handleIncrement = (counterId) => {
+    const updatedCounters = counters.map((counter) => {
+      if (counter.id === counterId) {
+        return {
+          ...counter,
+          value: counter.value + 1,
+        };
+      }
+      return counter;
+    });
+    setCounters(updatedCounters);
+  };
+  const handleDecrement = (counterId) => {
+    const updatedCounters = counters.map((counter) => {
+      if (counter.id === counterId) {
+        return {
+          ...counter,
+          value: counter.value - 1,
+        };
+      }
+      return counter;
+    });
+    setCounters(updatedCounters);
+  };
+  return (
+    <div
+      className="w-screen h-screen p-10 bg-gray-100
+  text-slate-700"
+    >
+      <h1
+        className="max-w-md mx-auto text-center text-2xl
+    font-bold"
+      >
+        Simple Counter Application
+      </h1>
+      <div className="max-w-md mx-auto mt-10 space-y-5">
+        {counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onIncrement={() => handleIncrement(counter.id)}
+            onDecrement={() => handleDecrement(counter.id)}
+            count={counter.value}
+          />
+        ))}
+        <Stats totalCount={totalCount} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
